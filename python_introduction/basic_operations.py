f78c580e9ece33fff0a3956d69616dc4e3acb69a
SmailAitHemmou <aithemmousmail@gmail.com>
2025-07-10 14:02:50 +0100

    update

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
 # This script demonstrates basic mathematical operations in Python.
-number1 = int (input ("Entre the first number : "))
-number2 = int (input("Entre the second number : "))
+number1 = 10
+number2 = 5
 # Define two numbers for our calculations
 print(f"Numbers chosen for calculations: number1 = {number1}, number2 = {number2}\n")
 
