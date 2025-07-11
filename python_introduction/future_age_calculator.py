4248b4ac7fc8bc5e902a112cdd622d24a6d887d0
SmailAitHemmou <aithemmousmail@gmail.com>
2025-07-12 00:11:37 +0100

    user_age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+  # User Input Age Calculator :
+user_age = int(input("How old are you? : "))
+  # use age in 2050 :
+user_age_in_future = user_age + 27
+print(f"in 2050, you will be {user_age_in_future} years old.")
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -2,4 +2,4 @@
 hours = 2
   # This code converts hours to seconds :
 hours_to_seconds = hours * 3600
-print(f"{hours} hour(s) is {hours_to_seconds} seconds")  
+print(f"{hours} hour(s) is {hours_to_seconds} seconds")
