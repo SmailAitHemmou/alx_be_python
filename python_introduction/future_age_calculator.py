fb5d46085875d4da6d72e827b138f76e510f5434
SmailAitHemmou <aithemmousmail@gmail.com>
2025-07-12 00:28:48 +0100

    up_user_age

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
   # User Input Age Calculator :
-user_age = int(input("How old are you? : "))
+user_age = int(input("How old are you?" ))
   # use age in 2050 :
 age = user_age + 27
 print(f"in 2050, you will be {age} years old.")
