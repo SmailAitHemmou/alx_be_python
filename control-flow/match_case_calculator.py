4e37bfb5995582bad730f4bb194ea21691315171
SmailAitHemmou <aithemmousmail@gmail.com>
2025-07-19 15:04:16 +0100

    commit09

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -9,9 +9,8 @@ match input_operator:
         print(f"The result is {num1 - num2}.")
     case '*':
         print(f"The result is {num1 * num2}.")
-    case '/' if num2 == 0:
-        print("Cannot divide by zero.")
-    case '/': 
-        print(f"The result is {num1 / num2}.")
-    case _:
-        print("Invalid operator.")
+    case '/':
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            print(f"The result is {num1 / num2}.")
