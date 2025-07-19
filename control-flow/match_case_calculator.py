c64ad0755d1b236e686ca6417e4f30bf9e910bec
SmailAitHemmou <aithemmousmail@gmail.com>
2025-07-19 15:47:11 +0100

    commit06

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,20 +1,23 @@
-# Simple Calculator with Match Case – No if/else used
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-input_operator = input("Choose the operation (+, -, *, /): ")
-# Handle zero division in operator choice
-if num2 == 0 and input_operator == '/':
-    result = "Cannot divide by zero."
-    match input_operator:
-        case '+':
-            result = f"The result is {num1 + num2}."
-        case '-':
-            result = f"The result is {num1 - num2}."
-        case '*':
-            result = f"The result is {num1 * num2}."
-        case '/':
-            result = f"The result is {num1 / num2}."
-        case _:
-            result = "Invalid operator."
+  # Simple Calculator with Match Case :
+num1= float(input("Enter first number: "))
+num2= float(input("Enter second number: "))
+operation = input("choose the operation (+, -, *, /): ")
 
-print(result)
+match operation:
+    case '+':
+        result = num1 + num2
+        print("The result is:", result)
+    case '-':
+        result = num1 - num2
+        print("The result is:", result)
+    case '*':
+        result = num1 * num2
+        print("The result is:", result)
+    case '/':
+        if num2 == 0:
+            print("cannot divide by zero")
+        else:
+            result = num1 / num2
+            print("The result is:", result)
+    case _:
+        print("Invalid operation")
