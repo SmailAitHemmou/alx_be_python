823f1cb0dcef7524a66fcf210d3311462cbaf06c
SmailAitHemmou <aithemmousmail@gmail.com>
2025-07-25 03:40:12 +0100

    up3_a-o-f

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,9 @@
   # Arithmetic Operations Function :
 
 def perform_operation(num1, num2, operation):
+    
+    operation = operation.lower()
+
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
