e8f4c5fd831d7bc8bf6e8922c756088e5eedd17d
SmailAitHemmou <aithemmousmail@gmail.com>
2025-07-25 02:16:32 +0100

    a-o-f

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+  # Arithmetic Operations Function :
+
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                print("cannot divide by zero")
+            else:
+                return num1 / num2
+        case _:
+            print("Invalid operation")
