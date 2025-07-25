4faa1a523a6fac3684e89d0ff11402a039b07f21
SmailAitHemmou <aithemmousmail@gmail.com>
2025-07-25 03:34:18 +0100

    up2_a-o-f

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
         return num1 * num2
     elif operation == 'divide':
         if num2 == 0 :
-            return None
+            return "Cannot divide by zero."
         return num1 / num2
     else:
-        print("Operation not recognized. Use 'add', 'subtract', 'multiply', or 'divide'.")
+        return "Operation not recognized. Use 'add', 'subtract', 'multiply', or 'divide'."
