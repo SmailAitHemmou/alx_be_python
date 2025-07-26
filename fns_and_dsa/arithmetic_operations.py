961561f98e938c90022f648e50015a06880e3133
SmailAitHemmou <aithemmousmail@gmail.com>
2025-07-26 16:13:16 +0100

    up11_a-o-f

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -13,7 +13,6 @@ def perform_operation(num1, num2, operation):
     elif operation == 'divide':
         if num2 == 0:
             return "Error: Division by zero"
-        else:
-            return num1 / num2
+        return num1 / num2
     else:
         return "Error: Invalid operation"
