c7f41e25df9e0567a11119353a2056cd1c005b6a
SmailAitHemmou <aithemmousmail@gmail.com>
2025-07-30 21:41:54 +0100

    up_s-b-a-c

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,11 +1,13 @@
   # Simple Bank Account Class :
 
-class BankAccount :
-    def __init__(self, initial_balance=0):
-        self.balance = initial_balance
+class BankAccount:
+
+    def __init__(self, account_balance = 0):
+        self.balance = account_balance
 
     def deposit(self, amount):
         if amount > 0:
+            amount = float(input("Enter deposit amount: "))
             self.balance += amount
         else:
             print("Deposit amount must be positive.")
@@ -13,6 +15,7 @@ class BankAccount :
     def withdraw(self, amount):
         if amount > 0 and amount <= self.balance:
             self.balance -= amount
+            amount = float(input("Enter withdrawal amount: "))
             return True
         else:
             print("Withdrawal amount must be positive and less than or equal to the current balance.")
