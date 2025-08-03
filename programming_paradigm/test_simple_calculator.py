12c79f52b03032bf35491ca28b9859cf5019962e
SmailAitHemmou <aithemmousmail@gmail.com>
2025-08-03 13:50:21 +0100

    s-c

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,37 @@
+  # Writing Unit Tests for a Simple Calculator Class :
+
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up a calculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(3, 5), 8)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 4), 6)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(5, 5), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(5, -1), -5)
+        self.assertEqual(self.calc.divide(0, 10), 0)
+
+    def test_divide_by_zero(self):
+        self.assertIsNone(self.calc.divide(10, 0))
+
+if __name__ == '__main__':
+    unittest.main()
