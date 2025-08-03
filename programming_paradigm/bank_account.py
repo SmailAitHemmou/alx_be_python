5fee2d55bb7b2f6eeda00dbaa3f764bec4a9978a
SmailAitHemmou <aithemmousmail@gmail.com>
2025-08-03 16:48:03 +0100

    up_s-b-a-c

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -23,4 +23,4 @@ class BankAccount:
             return True
 
     def display_balance(self):
-        print(f"Current account balance: ${self.account_balance:.2f}")
+        print(f"Current Balance:{self.account_balance:.2f}")
