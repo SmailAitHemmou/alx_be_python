13cc692f47568c83f610179bcfdb47cf347f5394
SmailAitHemmou <aithemmousmail@gmail.com>
2025-08-08 21:41:03 +0100

    i-c-p

---
+++ oop/library_system.py
@@ -0,0 +1,42 @@
+  # Inheritance and Composition in Python :
+
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+    def get_details(self) -> str:
+        return f"Book: {self.title} by {self.author}"
+
+class EBook(Book):
+    def __init__(self, title: str, author: str, file_size: int):
+        super().__init__(title, author)  
+        self.file_size = file_size
+
+    def get_details(self) -> str:
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+class PrintBook(Book):
+    def __init__(self, title: str, author: str, page_count: int):
+        super().__init__(title, author)  # Call base class constructor
+        self.page_count = page_count
+
+    def get_details(self) -> str:
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+class Library:
+    def __init__(self):
+        self.books = []  
+
+    def add_book(self, book: Book):
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            print("Error: Only Book instances can be added.")
+
+    def list_books(self):
+        if not self.books:
+            print("The library is empty.")
+        else:
+            for book in self.books:
+                print(book.get_details())
